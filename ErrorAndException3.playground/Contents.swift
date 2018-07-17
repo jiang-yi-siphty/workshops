641c2370d8568783b09f4ca0ae8c0aa46a93cfdd
Yi JIANG <jiang.yi@siphty.com>
2018-07-18 00:28:09 +1000

    Update: add new error type.

--- ErrorAndException3.playground/Contents.swift
+++ ErrorAndException3.playground/Contents.swift
@@ -1,4 +1,5 @@
 //: Error and Exception
+//:------------------------------------------------------------------------------
 
 import UIKit
 import PlaygroundSupport
@@ -8,66 +9,97 @@ PlaygroundPage.current.needsIndefiniteExecution = true
 //:Simple domain error
 //This is a kind of error we don't need know reason.
 //We just need to know it happened. And, we need do something.
-let num = Int("hello world")
+//------------------------------------------------------------------------------
+//let num = Int("hello world")
+//
+//let dic = ["a": 100, "b": 200]
+//let element0 = dic["a"]
+//let element1 = dic["key_not_exist"]
+//
+//func process() {
+//    guard let element2 = dic["key_not_exist"] else { return }
+//    print(element2)
+//}
 
-let dic = ["a": 100, "b": 200]
-let element0 = dic["a"]
-let element1 = dic["key_not_exist"]
 
-func process() {
-    guard let element2 = dic["key_not_exist"] else { return }
-    print(element2)
-}
-
-
-//Recoverable error
+//:Recoverable error
 // API request
-class ServerManager {
-    let fileUrl = URL(fileURLWithPath: NSTemporaryDirectory())
+//------------------------------------------------------------------------------
+//class ServerManager {
+//    let fileUrl = URL(fileURLWithPath: NSTemporaryDirectory())
+//
+//    func callAPI() {
+//        URLCache.shared.removeAllCachedResponses()
+//        let url = URL(string: "https://www.OneFakeUrl.com/0/api")!
+////        let url = URL(string: "https://api.ipify.org?format=json")!
+////        let url = URL(string: "http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=33.433638&lng=-112.008113&fDstL=0&fDstU=100")!
+//        let task = URLSession.shared.dataTask(with: url) { (data, response, error) -> Void  in
+//            if let error = error {
+//                // let user beware this error
+//                self.showErrorAlert("Network Error: \(error.localizedDescription)")
+//            } else if let data = data {
+//                let contents = NSString(data:data, encoding: String.Encoding.utf8.rawValue)
+//                print(contents!)
+//                self.write(data: data, to: self.fileUrl)
+//            }
+//        }
+//        task.resume()
+//    }
+//
+//    func showErrorAlert(_ errorMessage: String) {
+//        print(errorMessage)
+//    }
+//
+//    // Write file
+//    func write(data: Data, to url: URL) {
+//        do {
+//            try data.write(to: url)
+//        } catch let error as NSError {
+//            if error.code == NSFileWriteOutOfSpaceError {
+//                removeUnusedFiles()
+//                write(data: data, to: url)
+//            } else {
+//                // Other error, show error to user
+//                showErrorAlert("Write file Error: \(error.localizedDescription)")
+//            }
+//        } catch {
+//            showErrorAlert("Error: \(error.localizedDescription)")
+//        }
+//    }
+//
+//    func removeUnusedFiles() {
+//        //...
+//    }
+//}
+//
+//ServerManager().callAPI()
 
-    func callAPI() {
-        URLCache.shared.removeAllCachedResponses()
-        let url = URL(string: "https://www.OneFakeUrl.com/0/api")!
-//        let url = URL(string: "https://api.ipify.org?format=json")!
-//        let url = URL(string: "http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=33.433638&lng=-112.008113&fDstL=0&fDstU=100")!
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) -> Void  in
-            if let error = error {
-                // let user beware this error
-                self.showErrorAlert("Network Error: \(error.localizedDescription)")
-            } else if let data = data {
-                let contents = NSString(data:data, encoding: String.Encoding.utf8.rawValue)
-                print(contents!)
-                self.write(data: data, to: self.fileUrl)
-            }
-        }
-        task.resume()
-    }
 
-    func showErrorAlert(_ errorMessage: String) {
-        print(errorMessage)
-    }
+//:Universal error
 
-    // Write file
-    func write(data: Data, to url: URL) {
-        do {
-            try data.write(to: url)
-        } catch let error as NSError {
-            if error.code == NSFileWriteOutOfSpaceError {
-                removeUnusedFiles()
-                write(data: data, to: url)
-            } else {
-                // Other error, show error to user
-                showErrorAlert("Write file Error: \(error.localizedDescription)")
-            }
-        } catch {
-            showErrorAlert("Error: \(error.localizedDescription)")
-        }
-    }
+// 内存不足
+let a = [Int](repeating: 10, count: 3)
+print(a)
+//let b = [Int](repeating: 100, count: .max)
 
-    func removeUnusedFiles() {
-        //...
-    }
-}
+// 调用栈溢出
+//func foo() { foo() }
+//foo()
 
-ServerManager().callAPI()
+
+//:Logic failure
+//// 强制解包一个 `nil` 可选值
+//var name: String? = nil
+//name!
+//
+//// 数组越界访问
+//let arr = [1,2,3]
+//let num = arr[3]
+//
+//// 计算溢出
+//var a = Int.max
+//a += 1
+//
+//// 强制 try 但是出现错误
+//try! JSONDecoder().decode(Foo.self, from: Data())
 
